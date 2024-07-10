0ffff2dd35a14a18fa469ef2888e91953b592189
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-07-10 20:30:09 +0530

    Commit

--- string.js
+++ string.js
@@ -1,7 +1,7 @@
 const client = require("./client");
 
 async function init() {
-  await client.set("user:4", "huxn");
+  await client.set("user:4", "ayush");
   const result = await client.get("user:4");
   console.log("Result:", result);
 }
